59b497fa115af211d34826d633b2470cb5a4fc3e
僕は神だ <ajmalshahabudeen@gmail.com>
2024-08-12 18:47:21 +0530

    welcome page updated

--- app/page.tsx
+++ app/page.tsx
@@ -2,28 +2,47 @@
 import { Logo } from "@/components/MainHead/Logo";
 import { ThemeToggle } from "@/components/MainHead/ThemeToggle";
 import Help from "@/components/Other/Help";
-import { Box, Button } from "@chakra-ui/react";
+import { GetSession } from "@/utils/session";
+import { Box, Button, Text } from "@chakra-ui/react";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const route = useRouter();
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [username, setUsername] = useState<string>();
+
+  const fetchSession = async () => {
+    const session = await GetSession();
+    if (session?.user) {
+      setLoggedIn(true);
+      setUsername(session?.user?.name || "");
+    }
+  };
+  useEffect(() => {
+    fetchSession();
+  }, []);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <Box className="fixed top-0 flex w-full p-10 items-center justify-between gap-4">
         <Logo />
         <Box className="hidden md:inline-flex items-center gap-5">
-          <Button
-            variant={"link"}
-            size={"lg"}
-            onClick={() => route.push("/login")}
-          >
-            Login / Register
-          </Button>
+          {loggedIn ? (
+            <Text>{username}</Text>
+          ) : (
+            <Button
+              variant={"link"}
+              size={"lg"}
+              onClick={() => route.push("/login")}
+            >
+              Login / Register
+            </Button>
+          )}
           <Help />
           <ThemeToggle />
         </Box>
-        
+
         <Box className="flex md:hidden fixed bottom-0 right-0 pb-4 justify-center items-center w-full gap-5">
           <Button
             variant={"link"}
