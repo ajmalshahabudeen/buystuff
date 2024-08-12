de47c918a2d4c702613799ec25b1a8e6b5e1cecd
僕は神だ <ajmalshahabudeen@gmail.com>
2024-08-12 10:31:06 +0530

    theme bug fix

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,33 @@
-import { NextRequest } from "next/server"
-import authConfig from "./auth.config"
-import NextAuth from "next-auth"
- 
+import { NextRequest } from "next/server";
+import authConfig from "./auth.config";
+import NextAuth from "next-auth";
+
 // Use only one of the two middleware options below
 // 1. Use middleware directly
 // export const { auth: middleware } = NextAuth(authConfig)
- 
+
 // 2. Wrapped middleware option
-const { auth } = NextAuth(authConfig)
+const { auth } = NextAuth(authConfig);
 export default auth(async function middleware(req: any) {
   // Your custom middleware logic goes here
   // console.log("middleware", req.auth)
-  if (!req.auth && req.nextUrl.pathname !== "/login" && req.nextUrl.pathname !== "/") {
-    const newUrl = new URL("/login", req.nextUrl.origin)
-    // return Response.redirect(newUrl)
+  if (
+    !req.auth &&
+    (req.nextUrl.pathname === "/test" || req.nextUrl.pathname === "/")
+  ) {
+    const newUrl = new URL("/login", req.nextUrl.origin);
+    return Response.redirect(newUrl);
   }
-})
+  if (req.auth && req.nextUrl.pathname === "/login") {
+    const newUrl = new URL("/test", req.nextUrl.origin);
+    return Response.redirect(newUrl);
+  }
+});
+
+export const config = {
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+    "/api/:path*",
+    "/shop/:path*",
+  ],
+};
